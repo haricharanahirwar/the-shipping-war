fa1c43c9dd38f7b3c9c687cc1f1705c8f832b5de
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-07-12 13:56:59 +0530

    "Update url"

--- UI/src/API_URL.jsx
+++ UI/src/API_URL.jsx
@@ -1,9 +1,9 @@
-export const __userapiurl="http://localhost:3001/user/";
+export const __userapiurl="https://the-shipping-war-1.onrender.com/user/";
 
-export const __categoryapiurl="http://localhost:3001/category/";
+export const __categoryapiurl="https://the-shipping-war-1.onrender.com/category/";
 
-export const __subcategoryapiurl="http://localhost:3001/subcategory/";
+export const __subcategoryapiurl="https://the-shipping-war-1.onrender.com/subcategory/";
 
-export const __shipmentapiurl = "http://localhost:3001/shipment/";
+export const __shipmentapiurl = "https://the-shipping-war-1.onrender.com/shipment/";
 
-export const __bidapiurl="http://localhost:3001/bid/"
+export const __bidapiurl="https://the-shipping-war-1.onrender.com/bid/"
